e15039653793e71840c18355e57fa98bf5eac265
OlivierDupre <dupreolivier@gmail.com>
2013-01-22 23:43:49 +0100

    Trying to factorize code...

--- src/main/java/fr/dupre/RomanTranslator.java
+++ src/main/java/fr/dupre/RomanTranslator.java
@@ -1,11 +1,69 @@
 package fr.dupre;
 
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 /**
- * Classe de conversion d'un numérique en chiffre romain.
+ * Converts an arabian number into a roman number.
  */
 public class RomanTranslator {
 
-    Object convert(int i) {
-        return "I";
+    private enum RomanNumbers {
+
+        I("I", 1), V("V", 5), X("X", 10), L("L", 50), C("C", 100), D("D", 500),
+        M("M", 1000);
+        private final String romanDigit;
+        private final int arabicValue;
+
+        private RomanNumbers(String romanDigit, int arabicValue) {
+            this.romanDigit = romanDigit;
+            this.arabicValue = arabicValue;
+        }
+
+        public String getRomanDigit() {
+            return romanDigit;
+        }
+
+        public int getArabicValue() {
+            return arabicValue;
+        }
+
+        public static Set<Integer> arabicValues() {
+            Set<Integer> arabicValues = new TreeSet();
+
+            for (RomanNumbers romanNumber : values()) {
+                arabicValues.add(romanNumber.getArabicValue());
+            }
+
+            return arabicValues;
+        }
+    }
+
+    public String convert(int arabicNumber) {
+        TreeSet<Integer> romanNumberArabicValues = (TreeSet<Integer>) RomanNumbers.arabicValues();
+
+        for (Iterator<Integer> romanNumberIterator = romanNumberArabicValues.descendingIterator(); romanNumberIterator.hasNext();) {
+            int arabicValue = romanNumberIterator.next();
+        }
+
+        if (arabicNumber >= RomanNumbers.X.getArabicValue()) {
+            return convert(arabicNumber, RomanNumbers.X);
+        } else if (arabicNumber >= RomanNumbers.V.getArabicValue()) {
+            return convert(arabicNumber, RomanNumbers.V);
+        }
+
+        return convert(arabicNumber, RomanNumbers.I);
+    }
+
+    private String convert(int arabicNumber, RomanNumbers romanNumber) {
+        if (arabicNumber == 0) {
+            return "";
+        }
+
+        return romanNumber.getRomanDigit() + convert(arabicNumber - romanNumber.getArabicValue());
     }
 }
--- src/test/java/fr/dupre/RomanTranslatorTest.java
+++ src/test/java/fr/dupre/RomanTranslatorTest.java
@@ -1,12 +1,12 @@
 package fr.dupre;
 
-import org.testng.Assert;
+import static org.testng.Assert.*;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
- * Unit test for simple App.
+ * Tests cases for {@link RomanTranslator}.
  */
 public class RomanTranslatorTest {
 
@@ -24,6 +24,60 @@ public class RomanTranslatorTest {
 
     @Test
     public void convert1() {
-        Assert.assertEquals(romanTranslator.convert(1), "I");
+        assertEquals(romanTranslator.convert(1), "I");
+    }
+
+    @Test
+    public void convert2() {
+        assertEquals(romanTranslator.convert(2), "II");
+    }
+
+    @Test
+    public void convert3() {
+        assertEquals(romanTranslator.convert(3), "III");
+    }
+
+    @Test
+    public void convert5() {
+        assertEquals(romanTranslator.convert(5), "V");
+    }
+
+    @Test
+    public void convert6() {
+        assertEquals(romanTranslator.convert(6), "VI");
+    }
+
+    @Test
+    public void convert10() {
+        assertEquals(romanTranslator.convert(10), "X");
+    }
+
+    @Test
+    public void convert20() {
+        assertEquals(romanTranslator.convert(20), "XX");
+    }
+    
+
+    @Test
+    public void convert50() {
+        assertEquals(romanTranslator.convert(50), "L");
+    }
+    
+
+    @Test
+    public void convert100() {
+        assertEquals(romanTranslator.convert(100), "C");
+    }
+    
+
+    @Test
+    public void convert500() {
+        assertEquals(romanTranslator.convert(500), "D");
+    }
+    
+
+    @Test
+    public void convert1000() {
+        assertEquals(romanTranslator.convert(1000), "M");
     }
 }
